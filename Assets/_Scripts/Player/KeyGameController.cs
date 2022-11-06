f7dec498e5388e021b7f7b341e9c2621f922f449
진기 <jingit38@gmail.com>
2022-11-06 23:37:36 +0900

    SetInterface

---
+++ Assets/_Scripts/Player/KeyGameController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyGameController : IGameController
+{
+    public bool FireButtonPressed()
+    {
+        return Input.GetMouseButtonDown(0);
+        Debug.Log("KeyGame");
+    }
+}
---
+++ Assets/_Scripts/Player/MouseGameController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseGameController : IGameController
+{
+
+    public bool FireButtonPressed()
+    {
+        return Input.GetMouseButtonDown(0);
+        Debug.Log("GoGo!");
+    }
+}
